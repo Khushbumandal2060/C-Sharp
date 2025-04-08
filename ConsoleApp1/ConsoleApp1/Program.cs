8d44fb11d54d687b53eb17aa0ba3c1e4df9767a4
khushi <khusbumandal2060@gmail.com>
2025-04-09 01:53:00 +0545

    How to print height and width of rectangle

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Rectangle
+{
+    public float Width, Height;
+    public Rectangle(float width, float height)
+    {
+        Width = width;
+        Height = height;
+
+    }
+    public void Deconstruct(out float width, out float height)
+    {
+        width = Width;
+        height = Height;
+    }
+    static void Main(string[] args)
+    {
+        var rect = new Rectangle(3, 4);
+        (float width, float height) = rect; // Deconstruction
+        Console.WriteLine(width + " " + height);
+    }
+}
